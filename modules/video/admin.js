5da231b1346e114b00be3df82a441d3d2eca0755
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2015-06-04 10:34:12 -0400

    Created Video Block

---
+++ modules/video/admin.js
@@ -0,0 +1,33 @@
+(function($) {
+
+  /*$(".snowball-main").on("open", ".snowball-block-video", function() {
+    var zoom = $(this).find(".zoom").val();
+    $(this).find(".zoom-output").text(zoom);
+  });
+
+  $(".snowball-main").on("change keyup", ".snowball-block-video .video-url", function() {
+    var block = $(this).parents(".snowball-block-video");
+    var url = $(this).val();
+    var props = parseURL(url);
+
+    block.find("[data-target='video-url']").val(props.videoURL);
+    block.find("[data-target='frameSize'][value='" + props.frameSize +  "']").prop("checked", true);
+
+    block.trigger("render");
+  });
+
+  $(".snowball-main").on("input change", ".snowball-block-video .zoom", function() {
+    var block = $(this).parents(".snowball-block-video");
+    var zoom = $(this).val();
+
+    block.find(".zoom-output").text(zoom);
+  });
+
+  function parseURL(url) {
+    var re = /google.com\/maps\/.*@(-?\d+.\d+),(-?\d+.\d+),(\d+)([mz])/;
+    var matches = re.exec(url);
+
+  }*/
+
+ 
+})(jQuery);
