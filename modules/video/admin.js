e8fe2400328560eb33f91a6f2ab28c8a414e7359
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2015-06-05 13:34:57 -0400

    Video size changes based on the full width option.

--- modules/video/admin.js
+++ modules/video/admin.js
@@ -18,17 +18,34 @@
     block.find(".video-id").trigger("change");
   });
 
-  /*$(".snowball-main").on("click", ".snowball-block-video .frame-size", function() {
+  $(".snowball-main").on("click", ".snowball-block-video .full-width2", function() {
+    console.log("registered click!!!!!!!!");
     var block = $(this).parents(".snowball-block-video");
-    var selectedSize = $(this).val();
+    
+    console.log("full-width value: " + $(this).val());
+    console.log("is checked: " + $(this).prop("checked"));
+    var checkStatus = $(this).prop("checked");
+
+    if (checkStatus == false) {
+      block.find(".custom-style").val("");
+    } else {
+      block.find(".custom-style").val("width:100%; height:55.6vw;");
+    }
+
+    block.find(".video-url").trigger("keyup");
+
+    /*var selectedSize = $(this).val();
     var width = selectedSize.split("x")[0];
     var height = selectedSize.split("x")[1];
     
+    console.log("width: " + width);
+    console.log("height: " + height);
+
     block.find(".frame-width").val(width);
     block.find(".frame-height").val(height);
 
-    block.find(".video-url").trigger("keyup");
-  });*/
+    block.find(".video-url").trigger("keyup");*/
+  });
 
   function parseVideoURL(videoUrl) {
     var re = /youtube.com.*v=(.*)/;
