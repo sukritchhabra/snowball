1c9e27f6fad9cc6ddce5c5c577fcca9c3b308c09
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2015-06-17 17:22:53 -0400

    Table With Sorting Only On preview
    - Table has input spreadsheet on admin page
    - Template side displays Table without DataTable instance being created.
    - Bug: script cannot be loaded into the template.html <iframe>

--- modules/chart/admin.js
+++ modules/chart/admin.js
@@ -1,27 +1,62 @@
 (function($) {
-  $(document).ready(function() {
-
-    $(".snowball-main").on("open", ".snowball-block-chart", function() {
-      console.log("loaded");
+  $(".snowball-main").on("open", ".snowball-block-chart", function() {
+      var block = $(this);
       var container = $(this).find(".chart").get(0);
-
-      var data = [
-        ["", "Ford", "Volvo", "Toyota", "Honda"],
-        ["2014", 10, 11, 12, 13],
-        ["2015", 20, 11, 14, 13],
-        ["2016", 30, 15, 12, 13]
-      ];
+      var data = JSON.parse($(this).find("[data-target='JSON']").val());
 
       var hot = new Handsontable(container, {
         data: data,
-        minSpareRows: 1,
-        rowHeaders: true,
+        rowHeaders: false,
         colHeaders: true,
+        columnSorting: true,
         contextMenu: true,
+        persistantState: true,
+        afterChange: function() {
+          var generatedJSON = JSON.parse(JSON.stringify(data));
+          var jsonString = JSON.stringify(generatedJSON);
+
+          block.find("[data-target='JSON']").val(jsonString);
+          block.find("[data-target='HTML']").val(createTable(generatedJSON));
+        },
+        fillHandle: true,
+        manualColumnMove: true,
+        manualColumnResize: true,
+        multiSelect: true,
+        observeChanges: true,
+        search: true,
+        undo: true,
+        readOnly: false,
         stretchH: "all"
       });
 
     });
-  });
+
+
+    function createTable(JSON) {
+      var HTML = "<table>";
+      var numRows = JSON.length;
+
+      for(var i = 0; i < numRows; i++) {
+        if(i == 0) {
+          HTML = HTML + "<thead>";
+        } else if(i == 1) {
+          HTML = HTML + "</thead><tbody>";
+        }
+        HTML = HTML + "<tr>";
+        for(var j = 0; j < JSON[i].length; j++) {
+          if (i == 0) {
+            HTML = HTML + "<th>" + JSON[i][j] + "</th>";
+          } else {
+            HTML = HTML + "<td>" + JSON[i][j] + "</td>";
+          }
+        }
+
+        HTML = HTML + "</tr>";
+      }
+
+      HTML = HTML + "</tbody></table>";
+
+      return HTML;
+    }
 
 })(jQuery);
