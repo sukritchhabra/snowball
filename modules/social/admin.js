00f7474d0e68d329855e9ddf09ae60353b77849a
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2015-06-22 10:33:25 -0400

    Twitter follow button working, only on preview

--- modules/social/admin.js
+++ modules/social/admin.js
@@ -1,34 +1,66 @@
 (function($) {
   $(".snowball-main").on("open", ".snowball-block-social", function() {
     var block = $(this);
-    var url = $(this).find("[data-target='fbURL']").val();
-    console.log("found: " + JSON.stringify(url));
-    var HTML = "<div class=\"fb-like\" data-href=\"" + url + "\" data-layout=\"standard\" data-action=\"like\" data-show-faces=\"true\" data-share=\"true\"></div>\n";
+    var likeURL = $(this).find("[data-target='fbURL']").val();
+    var twitterUsername = $(this).find("[data-target='twitterUsername']").val();
+    console.log("found: " + JSON.stringify(likeURL));
+
+    var fbHTML = "<div class=\"fb-like\" data-href=\"" + likeURL + "\" data-layout=\"standard\" data-action=\"like\" data-show-faces=\"true\" data-share=\"true\"></div>\n";
+    var twitterHTML = "<a href=\"https://twitter.com/" + twitterUsername + "\" class=\"twitter-follow-button\" data-show-count=\"false\">Follow @" + twitterUsername + "</a>";
 
     if (block.find("[data-target='checkbox-fb']").is(":checked")) {
-      block.find(".snowball-facebook").val(HTML);
+      block.find(".snowball-facebook").val(fbHTML);
     } else {
       block.find(".snowball-facebook").val("");
+    }
+
+    if (block.find("[data-target='checkbox-twitter']").is(":checked")) {
+      block.find(".snowball-twitter").val(twitterHTML);
+    } else {
+      block.find(".snowball-twitter").val("");
     }
 
     block.trigger("render");
   });
 
   $(".snowball-main").on("click", "[data-target='checkbox-fb']", function() {
-
     console.log("click registered on fb checkbox");
     console.log("status: " + $(this).is(":checked"));
     var block = $(this).parents(".snowball-block-social");
-    var url = block.find("[data-target='fbURL']");
-    var HTML = "<div class=\"fb-like\" data-href=\"" + url + "\" data-layout=\"standard\" data-action=\"like\" data-show-faces=\"true\" data-share=\"true\"></div>\n";
-    
+    var likeURL = block.find("[data-target='fbURL']");
+    var fbHTML = "<div class=\"fb-like\" data-href=\"" + likeURL + "\" data-layout=\"standard\" data-action=\"like\" data-show-faces=\"true\" data-share=\"true\"></div>\n";
+
     if ($(this).is(":checked")) {
-      block.find(".snowball-facebook").val(HTML);
+      block.find(".snowball-facebook").val(fbHTML);
     } else {
       console.log("entering else");
       block.find(".snowball-facebook").val("");
     }
 
+    block.trigger("render");
+  });
+
+  $(".snowball-main").on("click keyup", "[data-target='checkbox-twitter']", function() {
+    var block = $(this).parents(".snowball-block-social");
+    var twitterUsername = block.find("[data-target='twitterUsername']").val();
+    var twitterHTML = "<a href=\"https://twitter.com/" + twitterUsername + "\" class=\"twitter-follow-button\" data-show-count=\"false\">Follow @" + twitterUsername + "</a>";
+
+    if ($(this).is(":checked")) {
+      block.find(".snowball-twitter").val(twitterHTML);
+    } else {
+      console.log("entering else");
+      block.find(".snowball-twitter").val("");
+    }
+
+    block.trigger("render");
+  });
+
+  $(".snowball-main").on("keyup", "[data-target='twitterUsername']", function() {
+    var block = $(this).parents(".snowball-block-social");
+    var twitterUsername = block.find("[data-target='twitterUsername']").val();
+    var twitterHTML = "<a href=\"https://twitter.com/" + twitterUsername + "\" class=\"twitter-follow-button\" data-show-count=\"false\">Follow @" + twitterUsername + "</a>";
+
+    block.find(".snowball-twitter").val(twitterHTML);
     block.trigger("render");
   });
 
