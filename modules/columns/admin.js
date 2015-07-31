a4e164a8676c207ffb8a8efcdf0d2728b6914d77
sukritchhabra <sukrit.chhabra@gmail.com>
2015-07-31 16:09:04 -0400

    Limiting columns to 4. Added delete option.
    * Delete option not added 1st column. (There should always be atleast
    one column)
    
    * User has to confirm deletion
    
    *Function created to activate buttons

--- modules/columns/admin.js
+++ modules/columns/admin.js
@@ -25,21 +25,32 @@
     $("#snowball-main").on("click", ".snowball-block-columns .add-button", function(event) {
       var block= $(this).closest(".snowball-block-columns");
       var currentCount = block.find(".editors .column-textarea").length;
-      addNewTextArea(block, (currentCount + 1));
+      if(currentCount <= 3) {
+        addNewTextArea(block, (currentCount + 1));
+      }
     });
+
+    $("#snowball-main").on("click", ".snowball-block-columns .fa-times", function(event) {
+      if(confirm("Are you yure you want to delete this column?")) {
+        var block = $(this).closest(".snowball-block-columns");
+        var button = $(this).closest(".toggle-button");
+        var selectedIndex = block.find(".toggle-buttons .toggle-button").index(button);
+
+        block.find(".column-textarea").eq(selectedIndex).remove();
+        block.find(".CodeMirror").eq(selectedIndex).remove();
+        block.find(".CodeMirror").eq((selectedIndex - 1)).show();
+        button.remove();
+        activateSelectedButton(block, (selectedIndex-1));
+      }
+    });
+
 
     $("#snowball-main").on("click", ".snowball-block-columns .toggle-button", function(event) {
       var block = $(this).closest(".snowball-block-columns");
       var selectedIndex = block.find(".toggle-buttons .toggle-button").index($(this));
 
-      block.find(".toggle-buttons .toggle-button").each(function(index, el) {
-        if(index === selectedIndex) {
-          $(el).addClass("active");
-        } else {
-          $(el).removeClass("active");
-        }
-      });
-      
+      activateSelectedButton(block, selectedIndex);
+
       block.find(".CodeMirror").each(function(index, el) {
         if(index === selectedIndex) {
           $(el).show();
@@ -54,17 +65,14 @@
       var buttonHTML = "";
 
       newHTML = newHTML + '<textarea class="column-textarea" data-target="col-' + columnNumber + '" style="height: 100%;">';
-      newHTML = newHTML + '  <div class="column column-' + columnNumber + '"></div>';
+      newHTML = newHTML + '<div class="column column-' + columnNumber + '"></div>';
       newHTML = newHTML + '</textarea>';
 
-
-      buttonHTML = buttonHTML + '<div class="toggle-button toggle-column-' + columnNumber + '">Column ' + columnNumber + '</div>';
+      buttonHTML = buttonHTML + '<div class="toggle-button toggle-column-' + columnNumber + '">Column ' + columnNumber + '<span class="fa fa-times"></span></div>';
 
       block.find(".editors").append(newHTML);           //Adding new Editor
       block.find(".toggle-buttons").append(buttonHTML); //Adding new Button
-
-      /* Trying to add data-target to template side */
-      //block.find(".snowball-preview").contents().find("body .row").append("{{col-" + columnNumber + "}}");
+      activateSelectedButton(block, (columnNumber - 1));
 
       var textarea = block.find(".editors .column-textarea:last-child");
       var editor = CodeMirror.fromTextArea(textarea[0], {
@@ -84,5 +92,15 @@
       });
 
       block.find(".CodeMirror:not(:last-of-type)").hide();  //Displaying only last added CodeMirror
+    }
+
+    function activateSelectedButton(block, selection) {
+      block.find(".toggle-buttons .toggle-button").each(function(index, el) {
+        if(index === selection) {
+          $(el).addClass("active");
+        } else {
+          $(el).removeClass("active");
+        }
+      });
     }
 })(jQuery);
