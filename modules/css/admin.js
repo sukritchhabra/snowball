0ca28701e97a626b9a602a59c3f367e753b815d3
sukritchhabra <sukrit.chhabra@gmail.com>
2015-08-07 16:20:41 -0400

    changin html block to css

---
+++ modules/css/admin.js
@@ -0,0 +1,25 @@
+(function($) {
+
+    $("#snowball-main").on("open", ".snowball-block-css", function() {
+
+      var block = $(this);
+      var textarea = block.find("textarea");
+
+      var editor = CodeMirror.fromTextArea(textarea[0], {
+        mode: "htmlmixed",
+        lineNumbers: true,
+        lineWrapping: true,
+        indentUnit: 2,
+        tabSize: 2,
+        theme: "monokai"
+      });
+
+      editor.setSize("100%", "100%");
+
+      editor.on("change", function() {
+        editor.save();
+        textarea.trigger("change");
+      });
+    });
+
+})(jQuery);
